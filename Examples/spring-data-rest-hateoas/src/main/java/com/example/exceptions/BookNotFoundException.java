4545390678a65d2d495c22b17176a87db8710229
Jefté Goes <jefte.goes.dev@outlook.com>
2024-12-17 23:59:06 -0300

    Updating example.

--- Examples/spring-data-rest-hateoas/src/main/java/com/example/exceptions/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.exceptions;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
